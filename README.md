f7ce4fd5c958822b90b82e8e0db8cc46a8a31139
Nao Yonashiro <owan.orisano@gmail.com>
2017-05-31 23:57:22 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# oproxypac
+Androidなどの細かいproxyの設定が可能でない環境向けの自動プロキシ設定ファイル配信用サーバを作るためのライブラリです.
+
+## Install
+```bash
+npm install oproxypac
+```
+
+## Example
+'http://localhost:8081/proxy.pac'でhttp通信のみlocal ipの8080で起動しているプロキシに転送する例
+```js
+const os = require("os");
+const ProxyPacServer = require("oproxypac");
+
+const IP = os.networkInterfaces().en0.find(x => x.family === "IPv4").address;
+const PROXY_PORT = 8080;
+const PORT = 8081;
+const proxyPacServer = new ProxyPacServer(`${IP}:${PROXY_PORT}`);
+
+proxyPacServer
+    .protocol("http")
+    .listen(PORT)
+    .then(() => {
+        console.info(`listen localhost:${PORT}`);
+    });
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const http = require("http");
+
+class ProxyPacServer {
+    constructor(proxyUrl) {
+        this.proxyUrl = proxyUrl;
+        this.protocols = [];
+    }
+    protocol(scheme, proxy) {
+        proxy = proxy || this.proxyUrl;
+        this.protocols.push({scheme, proxy});
+        return this;
+    }
+    listen(port) {
+        const server = http.createServer((req, res) => {
+            if (req.url !== "/proxy.pac") {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+            res.writeHead(200, {
+                "Content-Type": "application/x-ns/proxy-autoconfig"
+            });
+            const body = [
+                "function FindProxyForURL(url, host){",
+                this.protocols.map(x => `    if (shExpMatch(url, "${x.scheme}://*")) return "PROXY ${x.proxy}";`).join("\n"),
+                'return "DIRECT";',
+                "}",
+            ].join("\n");
+            res.end(body);
+        });
+        return new Promise((resolve, reject) => {
+            server.listen(port, resolve);
+        });
+    }
+}
+
+module.exports = ProxyPacServer;
